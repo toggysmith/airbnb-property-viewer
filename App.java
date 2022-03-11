86c6801491aeb7f127eb9af3503f49cee1e2ef5f
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-03-11 18:27:57 +0000

    refactor: add comments + documentation

--- App.java
+++ App.java
@@ -1,8 +1,21 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * App is the entry point of the program.
+ * 
+ * @author Adam Murray (K21003575)
+ * @author Augusto Favero (K21059800)
+ * @author Mathew Tran (K21074020)
+ * @author Tony Smith (K21064940)
+ * @version 1.0
+ */
 public class App extends Application
 {
+    /**
+     * Create the MainView which starts the application window.
+     * @param stage The JavaFX stage.
+     */
     @Override
     public void start(Stage stage) throws Exception
     {
--- MainController.java
+++ MainController.java
@@ -2,10 +2,17 @@ import javafx.scene.control.ComboBox;
 import javafx.fxml.FXML;
 import java.util.ArrayList;
 
+/**
+ * MainController hosts FXML GUI elements and onAction methods.
+ * 
+ * @author Adam Murray (K21003575)
+ * @author Augusto Favero (K21059800)
+ * @author Mathew Tran (K21074020)
+ * @author Tony Smith (K21064940)
+ * @version 1.0
+ */
 public class MainController
 {
     @FXML public ComboBox fromRangeBox;
     @FXML public ComboBox toRangeBox;
-    
-    
 }
