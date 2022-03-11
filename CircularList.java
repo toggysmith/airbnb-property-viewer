23f0cb796cbdac80eae9c89453b16c61f4aaefed
Murray, Adam (k21003575) <adam.m.murray@kcl.ac.uk>
2022-03-11 23:12:47 +0000

    Add files via upload

--- CircularList.java
+++ CircularList.java
@@ -1,62 +1,27 @@
-import java.util.ArrayList;
-import javafx.scene.layout.Pane;
-import java.util.List;
-
 /**
  * A circular list that has no end and no beginning, only the current position.
  *
  * @author Adam Murray K21003575
  * @version 11/03/2022
  */
-public class CircularList
+public interface CircularList<E>
 {
-    private List<Pane> list;
-    private int currentItem;
-
     /**
-     * Constructor for objects of class CircularList
+     * Gets the next element from the list.
+     * @return The next element in the list.
      */
-    public CircularList()
-    {
-        list = new ArrayList<>();
-        currentItem = 0;
-    }
+    public E getNext();
     
     /**
-     * Gets the next pane from the list.
-     * @return The next pane in the list.
+     * Gets the previous element from the list.
+     * @return The previous element in the list.
      */
-    public Pane getNext()
-    {
-        if (list.size() == 0)
-        {
-            return null;
-        }
-        currentItem = (currentItem + 1) % list.size();
-        return list.get(currentItem);
-    }
+    public E getPrev();
     
     /**
-     * Gets the previous pane from the list.
-     * @return The previous pane in the list.
+     * Adds an element to the list.
+     * The element is added so that it will appear after the previously added element.
+     * @param element The element to be added.
      */
-    public Pane getPrev()
-    {
-        if (list.size() == 0)
-        {
-            return null;
-        }
-        currentItem = list.size() - ((list.size() - currentItem) % list.size()) - 1;
-        return list.get(currentItem);
-    }
-    
-    /**
-     * Adds a pane to the list.
-     * The pane is added so that it will appear after the previously added pane.
-     * @param pane The pane to be added.
-     */
-    public void add(Pane pane)
-    {
-        list.add(pane);
-    }
+    public void add(E element);
 }
