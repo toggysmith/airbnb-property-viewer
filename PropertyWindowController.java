2f35917f9d792fa8af534c4eacd9e940c9e17a82
Adam-Murray <adam.m.murray@kcl.ac.uk>
2022-03-11 22:23:06 +0000

    Add property window fxml and its controller class

---
+++ PropertyWindowController.java
@@ -0,0 +1,16 @@
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXML;
+
+/**
+ * MainController hosts FXML GUI elements and onAction methods.
+ * 
+ * @author Adam Murray (K21003575)
+ * @author Augusto Favero (K21059800)
+ * @author Mathew Tran (K21074020)
+ * @author Tony Smith (K21064940)
+ * @version 1.0
+ */
+public class PropertyWindowController
+{
+    @FXML public GridPane fromRangeBox;
+}
