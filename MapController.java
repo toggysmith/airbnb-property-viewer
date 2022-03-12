f3377675b3c2912b2b2e0af8a5b4afe9224123d8
Adam-Murray <adam.m.murray@kcl.ac.uk>
2022-03-12 00:22:49 +0000

    Start of the implementation for creating the borough window
    Creates a borough window when a hexagon is clicked, and gets the name of the window from the hashmap that links to hexagons to text.
    
    However the click doesnt work properly and only opens the window when the very edge of a vertical line is clicked.

--- MapController.java
+++ MapController.java
@@ -9,52 +9,75 @@ import java.util.ResourceBundle;
 import javafx.scene.control.Label;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapController extends Pane
 {
     @FXML private AnchorPane hexagonTileMap;
     
+    private Map<Object, String> boroughNames; 
+
     private final static double r = 60.0;
     private final static double n = Math.sqrt(r * r * 0.75);
     private final static double TILE_WIDTH = 2 * n;
     private final static double TILE_HEIGHT = 2 * r;
     private final static double SEPARATION = 15;
-    
+
     @FXML
     public void initialize()
     {
+        boroughNames = new HashMap<>();
         createMap();
     }
-    
+
     private void createMap()
     {
         int rowCount = 7;
         int tilesPerRow = 7;
         int xStartOffset = 40;
         int yStartOffset = 40;
-        
+
         for (int x = 0; x < tilesPerRow; x++)
         {
             for (int y = 0; y < rowCount; y++)
             {
                 double xCoord = x * (TILE_WIDTH + SEPARATION) + (y % 2) * n + xStartOffset;
                 double yCoord = y * (TILE_HEIGHT * 0.75 + SEPARATION) + yStartOffset;
-                
-                if (y % 2 != 0) xCoord += SEPARATION / 2;
+
+                if (y % 2 != 0) {xCoord += SEPARATION / 2;}
+
+                VBox vbox = new VBox();
+                Label label = new Label("Southwark");
+                vbox.getChildren().add(label);
                 
                 Hexagon hexagon = new Hexagon(xCoord, yCoord);
                 hexagonTileMap.getChildren().add(hexagon);
-                
-                VBox vbox = new VBox();
+
                 hexagonTileMap.setLeftAnchor(vbox, xCoord);
                 hexagonTileMap.setTopAnchor(vbox, yCoord - TILE_HEIGHT * 0.25);
                 vbox.setPrefSize(TILE_WIDTH, TILE_HEIGHT);
                 vbox.setAlignment(Pos.CENTER);
                 hexagonTileMap.getChildren().add(vbox);
+
+                hexagon.setOnMouseClicked(e -> createBoroughWindow(boroughNames.get(hexagon)));
                 
-                Label label = new Label("Southwark");
-                vbox.getChildren().add(label);
+                boroughNames.put(hexagon, label.getText());
             }
+        }
+    }
+    
+    private void createBoroughWindow(String windowTitle)
+    {
+        try
+        {
+            new PropertyWindowView(windowTitle);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
