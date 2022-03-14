448391a23935d307bf916914f7b5e1b7f5dad763
Adam-Murray <adam.m.murray@kcl.ac.uk>
2022-03-14 11:33:35 +0000

    Added comments

--- ComboBoxOrderEnum.java
+++ ComboBoxOrderEnum.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Map;
 
 /**
- * Write a description of class ComboBoxSorts here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * This enum stores the values in the borough window ComboBox
+ * and the sorting info for those values
+ * 
+ * @author Adam Murray (K21003575)
+ * @author Augusto Favero (K21059800)
+ * @author Mathew Tran (K21074020)
+ * @author Tony Smith (K21064940)
+ * @version 14/03/2022
  */
 public enum ComboBoxOrderEnum
 {
@@ -18,18 +22,23 @@ public enum ComboBoxOrderEnum
     NUMBER_OF_REVIEWS_ASCENDING("Number of reviews (Ascending)", TableColumn.SortType.ASCENDING),
     NUMBER_OF_REVIEWS_DESCENDING("Number of reviews (Descending)", TableColumn.SortType.DESCENDING);
     
-    private String displayText;
-    private TableColumn.SortType type;
+    //Whether the table will be sorted in ascending or descending order.
+    private final TableColumn.SortType order;
+    private final String displayText;//The display text.
 
     /**
-     * Constructor for objects of class ComboBoxSorts
+     * Constructor for objects of class ComboBoxOrderEnum
      */
-    private ComboBoxOrderEnum(String displayText, TableColumn.SortType type)
+    private ComboBoxOrderEnum(String displayText, TableColumn.SortType order)
     {
         this.displayText = displayText;
-        this.type = type;
+        this.order = order;
     }
     
+    /**
+     * Returns a list of all the enums values.
+     * @return A list of all the enums values.
+     */
     public static List<ComboBoxOrderEnum> getAll()
     {
         List<ComboBoxOrderEnum> list = new ArrayList<>();
@@ -42,12 +51,21 @@ public enum ComboBoxOrderEnum
         return list;
     }
     
+    /**
+     * Over rides the toString method  to return text to be displayed on screen.
+     * @return The display text of the enum value.
+     */
+    @Override
     public String toString() {
         return displayText;
     }
     
+    /**
+     * Returns whether to sort the table in ascending or descending order.
+     * @return Whether to sort the table in ascending or descending order.
+     */
     public TableColumn.SortType getOrder()
     {
-        return type;
+        return order;
     }
 }
