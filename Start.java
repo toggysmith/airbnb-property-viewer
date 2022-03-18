a33f43d7533a590bb2787d502b663da13436659b
Toggy-Smith <tony.o.smith@kcl.ac.uk>
2022-03-18 11:06:22 +0000

    feat: add start method for bluej convenience

---
+++ Start.java
@@ -0,0 +1,30 @@
+// JavaFX
+import javafx.application.Application;
+// Project
+import model.App;
+
+/**
+ * A class to start the program (for convenience in BlueJ).
+ * 
+ * @author Adam Murray (K21003575)
+ * @author Augusto Favero (K21059800)
+ * @author Mathew Tran (K21074020)
+ * @author Tony Smith (K21064940)
+ * @version 1.0
+ */
+public class Start
+{
+    /**
+     * Launch the JavaFX application.
+     */
+    public static void start()
+    {
+        new Thread()
+        {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(App.class);
+            }
+        }.start();
+    }
+}
