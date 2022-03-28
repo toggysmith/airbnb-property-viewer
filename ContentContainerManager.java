79a2b64d5f96098566f35e2143306295bfc97d93
Toggy-Smith <tony.o.smith@kcl.ac.uk>
2022-03-28 17:09:38 +0100

    refactor: ContentContainerManager

--- ContentContainerManager.java
+++ ContentContainerManager.java
@@ -1,10 +1,12 @@
-import javafx.scene.layout.Pane;
-import javafx.fxml.FXMLLoader;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
 /**
- * Responsible for controlling the content pane currently shown in the center content container.
+ * Responsible for controlling the content pane currently shown in the content pane container. Also
+ * provides a way to get a content pane's controller by specifying the controller's class.
  *
  * @author Adam Murray (K21003575)
  * @author Augusto Favero (K21059800)
@@ -20,7 +22,17 @@ public class ContentContainerManager
     private final CircularList<Pane> circularList;
     
     /**
-     * Initialise the instance variables; load all the content panes;
+     * Retrieve an instance of a controller from its class.
+     * @param controllerClass The class of the controller wanted.
+     * @return The controller of a specific class.
+     */
+    public static Controller getController(Class controllerClass)
+    {
+        return classControllerMap.get(controllerClass);
+    }
+    
+    /**
+     * Initialise the instance variables, load all the content panes,
      * and set the first pane.
      */
     public ContentContainerManager(Pane contentContainer)
@@ -45,17 +57,6 @@ public class ContentContainerManager
     }
     
     /**
-
-     * Retrieve an instance of a controller from its class.
-     * @param controllerClass The class of the controller wanted.
-     * @return The controller of a specific class.
-     */
-    public static Controller getController(Class controllerClass)
-    {
-        return classControllerMap.get(controllerClass);
-    }
-    
-    /**
      * @return The previous pane.
      */
     public Pane getPrevious()
@@ -73,12 +74,22 @@ public class ContentContainerManager
 
     /*
      * Adds the given controller to the map.
+     * @param controller The controller to be added to the map.
      */
     private void addControllerToMap(Controller controller)
     {
         classControllerMap.put(controller.getClass(), controller);
     }
-
+    
+    /*
+     * Takes a list of content panes and for each one:
+     * -> Load the content pane using the path to its FXML file.
+     * -> Make the content pane's size always be the same size as the container it's in.
+     * -> Add the content pane to the circular list.
+     * -> Add the content pane's controller to the controller-class-to-controller map.
+     * @param paths A list of paths to each content pane's FXML file.
+     * @throws Exception Thrown if the FXML file cannot be loaded properly.
+     */
     private void loadContentPane(String ... paths) throws Exception
     {
         for (String path : paths)
@@ -86,13 +97,13 @@ public class ContentContainerManager
             FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
             
             Pane contentPane = loader.load();
-            Controller controller = loader.getController();
-            circularList.add(contentPane);
             
             contentPane.prefWidthProperty().bind(contentContainer.widthProperty());
             contentPane.prefHeightProperty().bind(contentContainer.heightProperty());
             
-            addControllerToMap(controller);
+            circularList.add(contentPane);
+            
+            addControllerToMap(loader.getController());
         }
     }
 }
