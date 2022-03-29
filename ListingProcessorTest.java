e7a0c471dc72fdc045d4a95231b053e937d7c99b
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-03-29 20:38:25 +0100

    test: add unit tests for `getListingWithId()`

--- ListingProcessorTest.java
+++ ListingProcessorTest.java
@@ -324,6 +324,58 @@ public class ListingProcessorTest
         
         assertEquals(maxPropertyPrice, 231);
     }
+    
+    /**
+     * Test that `getListingWithId()` throws an IllegalArgumentException with the right message when given a null listings argument.
+     */
+    @Test
+    public void testGetListingWithIdWithNullListings()
+    {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                    List<AirbnbListing> listings = null;
+
+                    ListingProcessor.getListingWithId(listings, "7833588");
+        });
+
+        assertEquals("The provided listings argument is invalid.", exception.getMessage());
+    }
+
+    /**
+     * Test that `getListingWithId()` throws an IllegalArgumentException with the right message when given an empty listings argument.
+     */
+    @Test
+    public void testGetListingWithIdWithEmptyListings()
+    {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                    List<AirbnbListing> listings = new ArrayList<>();
+
+                    ListingProcessor.getListingWithId(listings, "7833588");
+        });
+
+        assertEquals("The provided listings argument is invalid.", exception.getMessage());
+    }
+    
+    /**
+     * Test that `getListingWithId()` returns the correct list when given valid arguments.
+     */
+    @Test
+    public void testGetListingWithIdWithValidArguments()
+    {
+        List<AirbnbListing> listings = new ArrayList<>();
+        
+        AirbnbListing listing1 = new AirbnbListing("7483279", "Double beedroom in Southwark", "78372", "Adam", "Hammersmith and Fulham", 51.46977981, -0.189799402, "Entire home/apt", 160, 1, 0, "12/8/2015", 1.00, 1, 0);
+        
+        listings.add(new AirbnbListing("14403483", "Large room, sleeps 3, Brixton", "88550548", "Allison", "Lambeth", 51.47125306, -0.11250696, "Private room", 37, 2, 28, "03/03/2017", 4.12, 1, 254));
+        listings.add(new AirbnbListing("9957622", "Double bed in Notting Hill", "51168635", "Serge", "Westminster", 51.51782111, -0.192291889, "Private room", 35, 2, 18, "30/12/2016", 1.67, 1, 0));
+        listings.add(listing1);
+        listings.add(new AirbnbListing("2584302", "Epic room in LONDON!", "23426", "Jeff", "Hammersmith and Fulham", 51.21937781, -0.133779402, "Private room", 200, 1, 0, "12/8/2015", 1.00, 1, 0));
+        listings.add(new AirbnbListing("7833588", "Bright DOUBLE ROOM, central LONDON", "34472628", "Tommaso", "Hammersmith and Fulham", 51.52066587, -0.056124665, "Private room", 231, 1, 7, "07/12/2015", 0.37, 1, 0));
+        listings.add(new AirbnbListing("9020269", "Spacious Room next to Richmond Par", "47094767", "Marcus", "Kingston upon Thames", 51.41945318, -0.286341833, "Private room", 30, 1, 1, "09/10/2016", 0.65, 2, 35));
+        
+        final AirbnbListing matchingListing = ListingProcessor.getListingWithId(listings, "7483279");
+        
+        assertEquals(matchingListing, listing1);
+    }
 
     @Test
     public void testGetBoroughs() 
